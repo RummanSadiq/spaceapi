a9251051fde23fa9958e48686172214ccd59f2e6
Rumman Sadiq <chrumman@gmail.com>
2019-05-30 18:16:08 +0500

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -32,7 +32,9 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    { }
+    { 
+
+    }
 
 
     public function hasShop()
