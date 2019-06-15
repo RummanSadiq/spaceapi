63a4be2252db2782a8eaefae28e09858d8105d3e
Rumman Sadiq <chrumman@gmail.com>
2019-06-15 19:44:55 +0500

    Update NewMessage.php

--- app/Events/NewMessage.php
+++ app/Events/NewMessage.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class NewMessage implements ShouldBroadcast
+class NewMessage implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
